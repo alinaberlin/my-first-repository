45401bc9066f43ad1f22e7b9fe057458c414523f
Alina Ghetler <alina.ghetler@gmail.com>
2019-03-19 10:08:30 +0100

    adding homework

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+/* 18.03.2018
+
+Names and Input
+Create a variable hacker1 with the driver's name
+
+Print "The driver's name is XXXX"
+
+Create a variable hacker2 and ask the user for the navigator's name
+
+Print "The navigator's name is YYYY"
+
+Conditionals
+Depending on which name is longer, print:
+The Driver has the longest name, it has XX characters or
+Yo, navigator got the longest name, it has XX characters or
+wow, you both got equally long names, XX characters!!
+Loops
+Print all the characters of the driver's name, separated by a space and in capitals ie. "J O H N"
+
+Print all the characters of the navigator's name, in reverse order. ie. "nhoJ"
+
+Depending on the lexicographic order of the strings, print:
+
+The driver's name goes first
+Yo, the navigator goes first definitely
+What?! You both got the same name?
+Bonus Time!
+Ask the user for a new string and check if it's a Palindrome. Examples of palindromes:
+
+"A man, a plan, a canal, Panama!"
+"Amor, Roma"
+"race car"
+"stack cats"
+"step on no pets"
+"taco cat"
+"put it up"
+"Was it a car or a cat I saw?" and "No 'x' in Nixon".
+Go to lorem ipsum generator and:
+
+Generate 3 parragraphs. Store the text in a String
+Make your program count the number of words in the string
+Make your program count the number of times the latin word etappears
+Extra Resources
+prompt() - MDN | Ask user for input
+String - MDN
+if - MDN
+while - MDN
+for - MDN */
+
 let hacker1 = "Theresa"
 console.log("The driver's name is "+ hacker1)
 
